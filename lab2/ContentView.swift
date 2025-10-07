d5de118d0a047d9d9072d061a719302c4087a81c
James Mittler II <jamesmittlerii@my.smccd.edu>
2025-10-07 08:46:12 -0400

    first commit

--- lab2/ContentView.swift
+++ lab2/ContentView.swift
@@ -7,15 +7,45 @@
 
 import SwiftUI
 
+let images = ["Man1","Man2","Man3","Man4","Man5","Man6","Man7","Man8","Man9","Sou1","Sou2","Sou3","Sou4","Sou5","Sou6","Sou7","Sou8","Sou9"]
+
+
+struct TileCards: View {
+    var content: String
+    @State var isFaceUp: Bool = false
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundColor(.white)
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(lineWidth: 3).foregroundColor(.blue)
+           
+            Image(content).resizable().scaledToFit().padding()
+            let cover = RoundedRectangle(cornerRadius: 10)
+                .foregroundColor(.blue)
+            
+            cover.opacity( isFaceUp ? 0 : 1)
+            
+        }.padding(.horizontal)
+            .onTapGesture { isFaceUp = !isFaceUp }
+                
+            
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+            Text("Game").font(.largeTitle).foregroundColor(.blue)
+                .bold(true)
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 100), spacing: 1)]) {
+                ForEach(images[0..<images.count],
+                        id:\.self) {
+                    im in TileCards(content:im).aspectRatio(1, contentMode: .fit)
+                }
+            }
+            
         }
-        .padding()
     }
 }
 
