b08b15945b5947bab20d0c2834f2d1d452e7de28
James Mittler II <jamesmittlerii@my.smccd.edu>
2025-10-09 09:57:57 -0400

    prep for app store

--- lab2/ContentView.swift
+++ lab2/ContentView.swift
@@ -9,11 +9,12 @@ import AudioToolbox
 import GameKit
 import SwiftUI
 
-let allImages = [
-    "Man1", "Man2", "Man3", "Man4", "Man5", "Man6", "Man7", "Man8", "Man9",
-    "Sou1", "Sou2", "Sou3", "Sou4", "Sou5", "Sou6", "Sou7", "Sou8", "Sou9",
-    "Pin1", "Pin2", "Pin3", "Pin4", "Pin5", "Pin6", "Pin7", "Pin8", "Pin9",
-]
+let allImages: [String] = {
+    let suits = ["Man", "Sou", "Pin"]
+    return suits.flatMap { suit in
+        (1...9).map { "\(suit)\($0)" }
+    }
+}()
 
 struct Card: Identifiable {
     let id = UUID()
@@ -318,7 +319,7 @@ struct ContentView: View {
                     cards = newCards
                     indicesOfFaceUp = []
                     // Optional: subtle haptic to indicate mismatch
-                    playMismatchHaptic()
+                    //playMismatchHaptic()
                 }
             }
         }
